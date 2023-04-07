5e19d2eab2ce4d7b2079d59cf10847a4096951fe
lloydchang <lloydchang@gmail.com>
2023-04-07 07:16:28 +0000

    fix: docker-in-docker in codespaces

---
+++ .devcontainer/setup-script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# https://notes.alexkehayias.com/running-docker-compose-in-codespaces/
+
+# Assumes docker-compose.yaml is at the root /app root
+# cd app
+# This setup script depends on running some code in other containers
+dockerd &
+# Ugh yes there's no nice way to wait until dockerd is ready
+# sleep 5
+# Do whatever setup you need
+# docker-compose up -d
+# docker-compose exec {some command to run migrations etc}
+# If you need to use docker for this setup script you must stop all containers
+# otherwise, any servers running in docker will not have their ports
+# forwarded properly by codespaces.
+# ocker-compose stop
