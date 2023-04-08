948d5aa77757377caa4694a06654e7eba922802e
lloydchang <lloydchang@gmail.com>
2023-04-08 08:23:30 +0000

    fix: .devcontainer

--- .devcontainer/setup-script.sh
+++ .devcontainer/setup-script.sh
@@ -1,16 +1,19 @@
 #!/bin/sh -x
 
+cd $(dirname $0)
+
 # https://notes.alexkehayias.com/running-docker-compose-in-codespaces/
 
 # Assumes docker-compose.yaml is at the root /app root
 # cd app
 # This setup script depends on running some code in other containers
-sudo dockerd &
+# sudo dockerd &
 # Ugh yes there's no nice way to wait until dockerd is ready
-sleep 5
+# sleep 5
 cd ../app
+./run.sh
 # Do whatever setup you need
-docker-compose up -d
+# docker-compose up -d
 # docker-compose exec {some command to run migrations etc}
 # If you need to use docker for this setup script you must stop all containers
 # otherwise, any servers running in docker will not have their ports
---
+++ app/run.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -x
+
+cd $(dirname $0)
+
+# https://nodejs.org/en/docs/guides/nodejs-docker-webapp
+
+WHOAMI=$(whoami)
+
+docker build . -t $(whoami)/node-web-app
+
+docker images
+
+docker run -p 49160:8080 -d $WHOAMI/node-web-app
+
+# Get container ID
+docker ps
+
+# https://stackoverflow.com/questions/34496882/get-docker-container-id-from-container-name
+
+DOCKER_CONTAINER_ID=$(docker ps --format "{{.Names}}")
+
+docker ps -aqf "name=$DOCKER_CONTAINER_ID" | tail -1
+
+# Print app output
+docker logs $DOCKER_CONTAINER_ID
+
+# Example
+# Running on http://localhost:8080
+
+# Enter the container
+# docker exec -it $DOCKER_CONTAINER_ID /bin/bash
+
+docker ps
+
+curl -i localhost:49160
