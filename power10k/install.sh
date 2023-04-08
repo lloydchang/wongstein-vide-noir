2e9ef24390a3633b9e4dbf5a037ba718117aacd4
lloydchang <lloydchang@gmail.com>
2023-04-08 06:23:09 +0000

    feat: add power10k

---
+++ power10k/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -x
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+cp .zshrc ~/.zshrc
