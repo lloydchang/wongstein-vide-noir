7b1bbce6d670007c13ecbb5088bf7b6bc47868d6
lloydchang <lloydchang@gmail.com>
2023-04-08 07:07:58 +0000

    fix: powerlevel10k, devcontainer

--- powerlevel10k/install.sh
+++ powerlevel10k/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
+cd $(dirname $0)
+
 test -e ~/powerlevel10k || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 # echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
