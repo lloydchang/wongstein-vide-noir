cb5e7006e26ed11ffa99b04de4d210a779ab2cfb
lloydchang <lloydchang@gmail.com>
2023-04-08 19:07:21 -0700

    fix: powerlevel10k

--- powerlevel10k/install.sh
+++ powerlevel10k/install.sh
@@ -1,10 +1,9 @@
-#!/bin/sh -x
+#!/bin/sh -xe
 
 cd $(dirname $0)
 
-test -e ~/powerlevel10k || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+test -e ~/powerlevel10k || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k &
 
-# echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
-cp .zshrc ~/.zshrc
+cp .zshrc ~/.zshrc &
 cp .p10k.zsh ~/.p10k.zsh
+zsh
