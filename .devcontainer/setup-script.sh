e92a4025348e879da669be5b6b5197b88c7d3271
lloydchang <lloydchang@gmail.com>
2023-04-08 20:41:19 -0700

    fix: rm get-pip.py

--- .devcontainer/setup-script.sh
+++ .devcontainer/setup-script.sh
@@ -9,7 +9,7 @@ sudo pip install --upgrade pip &
 # https://stackoverflow.com/questions/62301268/whenever-i-try-to-install-torch-it-displays-killed
 # Mitigate "Killed" by adding "--no-cache-dir"
 sudo wget https://bootstrap.pypa.io/get-pip.py &
-sudo python get-pip.py &
+sudo $(python get-pip.py; rm get-pip.py) &
 sudo pip3 install --user -r ../notebooks/requirements.txt --no-cache-dir &
 sudo curl -fsSL https://deb.nodesource.com/setup_19.x | sudo bash - &
 sudo apt-get install -y nodejs &
